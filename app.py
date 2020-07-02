25e9a6ef8e9a8ec051bd1223c08977e12623a42a
Esteban Gadea <egadea@ixion.local>
2020-07-02 20:26:23 -0300

    added indicators

--- app.py
+++ app.py
@@ -22,6 +22,16 @@ import pandas as pd
 APP_PATH = str(pl.Path(__file__).parent.resolve())
 
 df = pd.read_csv(os.path.join(APP_PATH, os.path.join("data", "material.csv")))
+indicador_dict={"FF":[8.2,10],
+                    "HT":[3.1,4.2],
+                    "VBC":[3.8,5.4],
+                    "RM":[4.2,6.3],
+                    "ABT":[6.0,7.6]}
+indcolor_dict={"FF":["white","#fcccfa","#f734ef"],
+                    "HT":["#ed0000","#ed6f00","#eddd00"],
+                    "VBC":["#edce02","#03a843","#0476c7"],
+                    "RM":["#f5225a","#f58f22","#f5ee22"],
+                    "ABT":["#e8d900","#029e0c","#0032fa"]}
 
 app = dash.Dash(__name__)
 server = app.server
@@ -78,7 +88,7 @@ app.layout = html.Div(
                             marks={str(pipeta): str(pipeta) for pipeta in df['pipeta'].unique()},
                             step=None
                             ),
-                        ], style={'width': '45%', 'display': 'inline-block', 'margin-left':'30px'},
+                        ], style={'width': '300px', 'display': 'inline-block', 'margin-left':'30px'},
                     ),
                 html.Label(
                     [
@@ -91,7 +101,23 @@ app.layout = html.Div(
                             marks={str(matraz): str(matraz) for matraz in df['matraz'].unique()},
                             step=None
                             ),
-                        ], style={'width': '40%', 'display': 'inline-block', 'float':'right', },
+                        ], style={'width': '300px', 'display': 'inline-block', 'margin-left':'50px' },
+                    ),
+                html.Div(
+                    [
+                        html.Div(["Indicador"]),
+                        dcc.Dropdown(
+                            id='ind-dropdown',
+                            options=[
+                                {'label': 'Fenolftaleina', 'value': 'FF'},
+                                {'label': 'Heliantina', 'value': 'HT'},
+                                {'label': 'Verde de Bromocresol', 'value': 'VBC'},
+                                {'label': 'Rojo de Metilo', 'value': 'RM'},
+                                {'label': 'Azul de Bromotimol', 'value': 'ABT'}
+                            ],
+                            value='FF'
+                            ),
+                        ], style={'width': '200px', 'display': 'inline-block', 'margin-left':'50px' },
                     ),
                 ],
             ),
@@ -102,28 +128,30 @@ app.layout = html.Div(
 @app.callback(
     Output('graph-with-slider', 'figure'),
     [Input('pipeta-slider', 'value'),
-    Input('matraz-slider', 'value')])
-def update_figure(selected_pipeta, selected_matraz):
+    Input('matraz-slider', 'value'),
+    Input('ind-dropdown', 'value')])
+def update_figure(selected_pipeta, selected_matraz, selected_value):
 
     f_vol = np.arange(1,25,0.2)
     f_ph = qa_utils.construct_curve(4.76, 0.8326394*selected_pipeta/selected_matraz)
-    #fig = px.line(y=f_ph, x=f_vol, color='black')
     fig = go.Figure(data=go.Scatter(y=f_ph, x=f_vol,line_color='black'))
-
-    fig.add_shape(
+    fig.add_shape(go.layout.Shape(
         # filled Rectangle
+            name='lower',
             type="rect",
             x0=1,
-            y0=2.5,
+            y0=2,
             x1=24.8,
             y1=8.2,
             fillcolor="white",
             layer='below',
             line_width=0
         )
+        )
 
-    fig.add_shape(
+    fig.add_shape(go.layout.Shape(
             # filled Rectangle
+            name='middle',
             type="rect",
             x0=1,
             y0=8.2,
@@ -133,8 +161,10 @@ def update_figure(selected_pipeta, selected_matraz):
             layer='below',
             line_width=0
             )
-    fig.add_shape(
+            )
+    fig.add_shape(go.layout.Shape(
             # filled Rectangle
+            name='higher',
             type="rect",
             x0=1,
             y0=10,
@@ -144,13 +174,24 @@ def update_figure(selected_pipeta, selected_matraz):
             layer='below',
             line_width=0
             )
+            )
+    fig.update_shapes(dict(y1=indicador_dict[selected_value][0],
+                            fillcolor=indcolor_dict[selected_value][0]),
+                        selector=dict(y0=2))
+    fig.update_shapes(patch=dict(y0=indicador_dict[selected_value][0],
+                                y1=indicador_dict[selected_value][1],
+                                fillcolor=indcolor_dict[selected_value][1]),
+                            selector=dict(name='middle'))
+    fig.update_shapes(patch=dict(y0=indicador_dict[selected_value][1],
+                                fillcolor=indcolor_dict[selected_value][2]),
+                            selector=dict(name='higher'))
     fig.update_xaxes(showline=True,
                     linewidth=2,
                     linecolor='black')
     fig.update_yaxes(showline=True,
                     linewidth=2,
                     linecolor='black')
-    fig.update_layout(transition_duration=500,
+    fig.update_layout(transition_duration=5,
                         xaxis_title = "Volumen de base (ml)",
                         yaxis_title = "pH",
                         xaxis_showgrid = False,
